6c5172a02750fafc443db96673b92153b46f5cbf
Terramet <jbamfo876@gmail.com>
2018-10-16 16:19:24 +0100

    Added server and client sockets, for master and slaves

--- app.js
+++ app.js
@@ -5,8 +5,43 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var io = require('socket.io').listen(3000);
+var server = require('http').Server(app);
+var socket = io.sockets;
 var app = express();
+
+//server.listen(3000);
+// WARNING: app.listen(80) will NOT work here!
+
+var connectedUserMapSlave = new Map();
+var connectedUserMapMaster = new Map();
+
+socket.on('connect', function(socket){
+  console.log('a user connected');
+  let connectedUserId = socket.id;
+
+  socket.on('recieveUserNameSlave', function(data){
+    //find user by there socket in the map the update name property of value
+    connectedUserMapSlave.set(socket.id, { status:'online', name: 'none' });
+    let user = connectedUserMapSlave.get(connectedUserId);
+    user.name = data.name + connectedUserId;
+    console.log(connectedUserMapSlave)
+  });
+
+  socket.on('recieveUserNameMaster', function(data){
+    //find user by there socket in the map the update name property of value
+    connectedUserMapMaster.set(socket.id, { status:'online', name: 'none' });
+    let user = connectedUserMapMaster.get(connectedUserId);
+    user.name = data.name + connectedUserId;
+    console.log(connectedUserMapMaster)
+  });
+
+  socket.on('disconnect', function () {
+    console.log('a user disconnected')
+    connectedUserMapSlave.delete(connectedUserId)
+    connectedUserMapMaster.delete(connectedUserId)
+  });
+});
 
 //implementation to accept text/plain
 app.use(function(req, res, next){
